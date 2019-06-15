4794de163516ceeafd8463b39b9cccf2fa140872
DinaTV <dina.vintr@gmail.com>
2019-06-15 14:35:00 +0300

    Many fixes.

--- scripts/loadGigya.js
+++ scripts/loadGigya.js
@@ -1,18 +1,11 @@
-loadGigya();
+import {clientCfg} from "./configuration.js";
 
 function loadGigya() {
-    var url = new URL(window.location.href);
-    var apiKeyParam = url.searchParams.get("apiKey");
-    var domainParam = url.searchParams.get("domain");
-
-    if (apiKeyParam) {
-        g_apiKey = apiKeyParam;
-    }
-    if (domainParam) {
-        g_domain = domainParam
-    }
-
-    var gigurl = ("https://cdns." + g_domain + ".gigya.com/js/socialize.js?apikey=" + g_apiKey);
-    document.write('<scr' + 'ipt type="text/javascript" src="' + gigurl + '"></scr' + 'ipt>');
- 
+    var gigurl = `https://cdns.${clientCfg.domain}.gigya.com/js/socialize.js?apikey=${clientCfg.apiKey}`;
+	var eScript = document.createElement("script");
+	eScript.src = gigurl;
+	document.head.appendChild(eScript);
+	
 }
+
+loadGigya();
