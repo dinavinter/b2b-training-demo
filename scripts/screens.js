f48650c41e824b574ef72f0e26b441446a77cbab
dina.vi <dina.vi@gigya-inc.com>
2019-06-15 14:39:14 +0300

    screens staf

--- scripts/screens.js
+++ scripts/screens.js
@@ -1,30 +1,7 @@
-function errorHandler(e) {
-    console.log(e);
-}
 
-
-
-function showGetAccountInfoResponse(response) {
-    document.getElementById('div').innerHTML = "<pre>"+JSON.stringify(response, undefined, 2)+"</pre>";
- }
-
- function showResponse(eventObj) {
-    if (eventObj.response.errorCode == 0) {
-        document.getElementById('div').innerHTML = "<center> Request submitted</center>";
-    }
-}
-
-
-function showLoginResponse(eventObj) {
-    if (eventObj.response.errorCode == 0) {
-        document.getElementById('div').innerHTML = "<center>Logged in UID: "+ eventObj.response.UID+"</center>";
-    }
-}
 function logout() {
     gigya.socialize.logout({onError: errorHandler, callbac:showGetAccountInfoResponse})
 }
-
-
 
 function showProfile() {
     var params = {
@@ -32,45 +9,114 @@ function showProfile() {
         containerID: "div",
         startScreen: "gigya-update-profile-screen",
         conflictHandling: "saveProfileAndFail"
-    }
+    };
     gigya.accounts.showScreenSet(params);
 }
  
 function showAccountJson() { 
-    gigya.accounts.getAccountInfo({ callback:showGetAccountInfoResponse, include: "groups, *" });
+    gigya.accounts.getAccountInfo({ callback:showResponse, include: "groups, *" });
+
+    function showResponse(response) {
+        document.getElementById('div').innerHTML = "<pre>"+JSON.stringify(response, undefined, 2)+"</pre>";
+    }
 
 }
 
+async function showRegistration(containerID) {
 
-function showGetUserInfoJson() { 
-    gigya.socialize.getUserInfo({ callback:showGetAccountInfoResponse })
-};
- 
-function showRegistration() {
-    var params = {
-        screenSet: "Default-RegistrationLogin",
-        containerID: "div",
-        startScreen: "gigya-register-screen",
-        onAfterSubmit: showLoginResponse
+    return new Promise((resolve, reject) => {
+        var params = {
+            screenSet: "Default-RegistrationLogin",
+            containerID: containerID,
+            startScreen: "gigya-register-screen",
+            include:"groups",
+            onAfterSubmit: r=>onCallback(r, resolve, reject)
 
-    }
-    gigya.accounts.showScreenSet(params);
+        };
+        gigya.accounts.showScreenSet(params);
+
+    });
+
+    function onCallback(response, resolve, reject){
+        if ( response.errorCode != 0) {
+            reject(response);
+            return;
+        }
+        var account= loadAccount().catch(reject);
+        account .then(r=>setAccountVariables(r));
+        account .then(r=>resolve(r));
+
+     }
+
 }
 
  
 function showSelfRegistration() {
     var params = {
         screenSet: "Default-OrganizationRegistration",
-        containerID: "div", 
+        containerID: "div",
         onAfterSubmit: showResponse
 
-    }
+    };
     gigya.accounts.showScreenSet(params);
+
+    function showResponse(eventObj) {
+        if (eventObj.response.errorCode == 0) {
+            document.getElementById('div').innerHTML = "<center> Request submitted</center>";
+        }
+    }
 }
 
 
 function openDelegatedAdmin() {
     gigya.accounts.b2b.openDelegatedAdminLogin({orgId:g_orgId});
 }
+
+
+ async function loadAccount() {
+     return new Promise((resolve, reject) => {
+         setTimeout(() => {
+             gigya.accounts.getAccountInfo({ callback:r=>onCallback(r, resolve, reject), include: "groups, *" });
+         }, 2000)
+     });
+
+     function onCallback(response, resolve, reject){
+         if ( response.errorCode != 0) {
+             reject(response);
+             return;
+         }
+         setAccountVariables(response);
+         resolve(response);
+     }
+}
+
+function setAccountVariables(response) {
+        if ( response.errorCode != 0) {
+            return;
+        }
+
+        var uid= response.UID;
+        var org = response["groups"]["organizations"][0];
+
+        setCookie("uid", uid);
+        setCookie("orgId", org.orgId);
+
+    }
+
+
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+
+function errorHandler(e) {
+    console.log(e);
+}
+
+
+
 
 
