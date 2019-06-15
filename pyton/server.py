efeeddc8fd6d2fe924239f004bf67482571a9fde
DinaTV <dina.vintr@gmail.com>
2019-06-15 15:41:09 +0300

    fix it

--- pyton/server.py
+++ pyton/server.py
@@ -1,7 +1,10 @@
 import BaseHTTPServer, SimpleHTTPServer
 import ssl
+import os
+
+os.chdir("..\\")
 
 httpd = BaseHTTPServer.HTTPServer(('localhost', 1443), SimpleHTTPServer.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, server_side=True,
-                                certfile='yourpemfile.pem')
+                                certfile='pyton\\yourpemfile.pem')
 httpd.serve_forever()
