116d1d8eda994cb87ae8d9565186b7a32691cbdd
dina.vi <dina.vi@gigya-inc.com>
2019-06-19 23:16:12 +0300

    install bat

---
+++ auth-server/install.bat
@@ -0,0 +1,2 @@
+python -m pip install --upgrade pip --trusted-host pypi.org --trusted-host files.pythonhosted.org
+pip install -r requirements.txt --trusted-host pypi.org --trusted-host files.pythonhosted.org
