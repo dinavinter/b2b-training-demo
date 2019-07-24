4313d0d5037d2de047103c6d5a2ec02b5c315c49
dina.vi <dina.vi@gigya-inc.com>
2019-07-24 23:15:18 +0300

    fix certificate and jwt to ptyon 3

--- server/client_jwt.py
+++ server/client_jwt.py
@@ -1,28 +1,31 @@
 # pip install jwt requests
 import jwt
 import requests
-import time
+import time 
+import base64
 
 def create_client_jwt(client_id, client_secret):
     iat = int(time.time())
     message = {
         'iss': client_id,
         'iat': iat,
-        'exp': iat + 300,
+        'exp': iat + 500,
     }
-
-    encoded_jwt = jwt.encode(message, client_secret.decode("base64"), algorithm='HS256')
-    return encoded_jwt
+ 
+    header = {"alg":"HS256","typ":"JWT"}
+    b64_string = base64.b64decode(client_secret)
+    encoded_jwt = jwt.encode(message, b64_string  , algorithm='HS256' , headers= header)
+    return  str(encoded_jwt, 'utf-8')
     
 def main():
-    CLIENT_ID = "OvHHZLi5dcoVQP3iR60e2LHTUPuFPxJ8"
-    CLIENT_SECRET = "5nAHJTrNukzEpOA74snLmRLWQkhvGOwI"
-    API_KEY = "3_kkSKeD-UGbjbWrnnXD1nl7xOsitRcHhjUGlYNzVTH3wFLddMDnNXPVARspAdJ5O6"
+    CLIENT_ID = "0evun2M3mvCW6uuuIMIVBc8JYEIt2wCd"
+    CLIENT_SECRET = "E5T8O8jPyfJwRJPiJvxZ2sgthhsGrWJE"
+    API_KEY = "3_OnQvOXVIpn97YPJP6MYSMHLtYjuX0cusTWBfnm2PWJiO959CQOyMdJECTJJpwl3K"
 
-    UID = "a46c64b44c4a4611ad6e35f5b644014f"
-    ORG_ID = "fe11eaff-695a-4b09-8324-3ed35723ae54"
+    UID = "78302ee6d8a141f08c9efdba55787ad3"
+    ORG_ID = "159a07a3-09a7-4b2b-8527-445c910689d4"
 
-    APP_ID = "1NOAHBNU8J0AJUENVRC9"
+    APP_ID = "V8DP1YJFD2TCAHYF02SX"
     
     body = {
         "identity": {
@@ -35,8 +38,16 @@ def main():
     
 
     URL = "https://us1api.b2b-gigya.com/runtime/%s/authorization/token/%s" % (API_KEY, APP_ID)
-    resp = requests.post(URL, json=body, headers={"Authorization": "Bearer %s" % encoded_jwt})
-    print(resp)
+    encoded_jwt = create_client_jwt(CLIENT_ID, CLIENT_SECRET)
+    header = "Bearer %s" % (encoded_jwt)
+
+    print(URL)
+    print(body) 
+    print(header) 
+    
+
+    resp = requests.post(URL, json=body, headers={"Authorization": header})
+    
     print(resp.text)
     
 if __name__ == "__main__":
